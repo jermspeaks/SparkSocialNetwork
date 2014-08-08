85b840373a5b5b6f12fbeee9387bca08df9afb0e
gond0026@umn.edu <gond0026@umn.edu>
2014-08-08 14:37:14 -0500

    for quy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,6 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      flash[:success] = "Welcome to FLIGHT"
       redirect_to @user
     else
       render 'new'
@@ -24,16 +23,24 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
     @user = User.find(params[:id])
-    @user.update_attributes(params[:user])
+
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
 
   def delete
     @user = User.find(params[:id])
     @user.destroy!
+
+    redirect_to users_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  root 'home#index'
-  get 'home/index'
+  root 'users#new'
+  get 'users/new'
 
   resources :users
-
   resources :photos
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

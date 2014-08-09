8fc690d34fa36dcca5e6be5cbf2fe5fc8cc801a4
Jeremy <jermspeaks@gmail.com>
2014-08-09 18:36:17 -0500

    deleted updates

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,6 @@ class Post < ActiveRecord::Base
   belongs_to :poster, class_name: "User"
   has_many :post_photos
   has_many :photos, through: :post_photos
-  has_many :updates
   has_many :comments
 
   validates_presence_of :poster

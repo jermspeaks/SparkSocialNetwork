6f3c278ed881de5b8a45dcb5bf44dcd3ff3bbc86
jermspeaks <jermspeaks@gmail.com>
2014-08-09 20:57:31 -0500

    comments fixed

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,6 +3,7 @@ class CommentsController < ApplicationController
     @user = User.find(session[:user_id])
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create(comment_params)
+    @comment.update(commenter: @user)
     redirect_to post_path(@post)
   end
 
